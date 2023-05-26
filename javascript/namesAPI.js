cbe0b60987028e77d510d9f307032aaabada17bd
CODZ <105626276+codz-dev@users.noreply.github.com>
2023-05-26 23:06:45 +0000

    Create namesAPI.js
    API file

---
+++ javascript/namesAPI.js
@@ -0,0 +1,28 @@
+const axios = require('axios');
+const config = require('./config');
+
+class NamesAPI {
+  constructor() {
+    this.api_url = 'https://namesapi.online/api';
+    this.api_key = config.api_key;
+  }
+
+  getGender(name) {
+    const url = `${this.api_url}`;
+    const headers = {
+      'Content-Type': 'application/json',
+      'API-Key': this.api_key
+    };
+    const data = {
+      name: name
+    };
+
+    return axios.post(url, data, { headers })
+      .then(response => response.data)
+      .catch(error => {
+        throw new Error(`Une erreur s'est produite lors de la requête : ${error.message}`);
+      });
+  }
+}
+
+module.exports = NamesAPI;
