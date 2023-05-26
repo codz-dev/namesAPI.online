95f57a2a3df162d2a9b8203a9af007e45a0519fe
CODZ <105626276+codz-dev@users.noreply.github.com>
2023-05-26 22:58:16 +0000

    Create namesAPI.php
    API file

---
+++ php/namesAPI.php
@@ -0,0 +1,59 @@
+<?php
+
+class NamesAPI {
+    private $config;
+    private $apiUrl = 'https://namesapi.online/api';
+    
+    public function __construct($configFile = 'config.php') {
+        $this->loadConfig($configFile);
+    }
+    
+    private function loadConfig($configFile) {
+        if (!file_exists($configFile)) {
+            throw new Exception('Config file not found');
+        }
+        
+        $this->config = require $configFile;
+    }
+    
+    public function getGender($name) {
+        $url = $this->apiUrl . '/api';
+        
+        $data = [
+            'name' => $name
+        ];
+        
+        $headers = [
+            'Content-Type: application/json',
+            'API-Key: ' . $this->config['api_key']
+        ];
+        
+        $ch = curl_init();
+        
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        
+        $response = curl_exec($ch);
+        $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        
+        curl_close($ch);
+        
+        if ($httpCode === 200) {
+            $result = json_decode($response, true);
+            return [
+                'gender' => $result['gender'],
+                'probability' => $result['probability']
+            ];
+        } else {
+            return [
+                'gender' => 'Unknown',
+                'probability' => null
+            ];
+        }
+    }
+}
+
+?>
