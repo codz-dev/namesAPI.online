cd5860b71a54a3bf4195d628c8c465dd53d2c3f1
CODZ <105626276+codz-dev@users.noreply.github.com>
2023-05-27 03:06:17 +0000

    Update namesAPI.py

--- python/namesAPI.py
+++ python/namesAPI.py
@@ -3,24 +3,32 @@ import requests
 class NamesAPI:
     def __init__(self):
         self.api_url = 'https://namesapi.online/api'
-        self.api_key = self.get_api_key()
+        self.api_key = self.getApiKey()
 
-    def get_api_key(self):
-        # Importer la clé d'API depuis le fichier de configuration
+    def getApiKey(self):
+        # Import API key from config.py
         from config import api_key
         return api_key
 
-    def get_gender(self, name):
+    def getGender(self, name):
         url = f'{self.api_url}'
         headers = {
             'Content-Type': 'application/json',
-            'API-Key': self.api_key
+            'X-API-Key': self.api_key
         }
         data = {
             'name': name
         }
-
+        
         response = requests.post(url, headers=headers, json=data)
+        if response.status_code != 200:
+            return {
+                'error': 'An error occured while calling the NamesAPI'
+            }
         response_data = response.json()
 
         return response_data
+       
+            
+
+        
